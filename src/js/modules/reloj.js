160a172ca7be3b295fd06df271e2a584780aee02
DANIELXCERON <43451510+DANIELXCERON@users.noreply.github.com>
2021-03-02 17:08:50 -0500

    cambio nombre de archivo logger
    Se cambio la forma en la que se escribe el nombre del archivo de log, ahora el formato es Año-NombreDelMes.log.

--- src/js/modules/reloj.js
+++ src/js/modules/reloj.js
@@ -101,7 +101,7 @@ function gT(Iwant) {
 
   var DateDMYYYY = nTF.checkFormat(dd) + "/" + nTF.checkFormat(mm + 1) + "/" + yyyy;
 
-  var DateLog = `${yyyy}-${nTF.checkFormat(mm + 1)}-${nTF.checkFormat(dd)}`;
+  var DateLog = `${yyyy}-${mes[mm]}`;
 
   switch (Iwant) {
     // "minutos y segundos en 00:00"
